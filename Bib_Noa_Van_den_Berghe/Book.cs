c0f40bf9ae24a3b790822832fbee11039dea35e8
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-03-05 14:32:22 +0100

    Add project files.

---
+++ Bib_Noa_Van_den_Berghe/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bib_Noa_Van_den_Berghe
+{
+    internal class Book
+    {
+		private string Titel;
+
+		public string titel
+		{
+			get { return Titel; }
+			set { Titel = value; }
+		}
+	}
+}
---
+++ Bib_Noa_Van_den_Berghe/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bib_Noa_Van_den_Berghe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
